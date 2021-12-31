6e59afcb1d8385cc23e75a64a2e471891f9ef654
Mark <verhovcev214265@gmail.com>
2021-12-31 19:49:11 +0200

    Merry Christmas

---
+++ src/happy_new_year/Friend.java
@@ -0,0 +1,26 @@
+package happy_new_year;
+public class Friend {
+
+    private static final String WISH = " I wish you a Merry Christmas and a Happy New Year =) ";
+    private static final String SPACE = " ";
+    private static final String INCORRECT_MESSAGE = "Incorrect Symbol";
+    private static final String YELLOW_PRINT = "\u001B[33m";
+
+    private String firstName;
+
+    public String greeting(int input){
+        try {
+            if (input == 1){
+                firstName = YELLOW_PRINT + "Valentin";
+            }else if (input == 2){
+                firstName = YELLOW_PRINT + "Elijah";
+            }else {
+                System.out.println(YELLOW_PRINT + "I don`t know who are you, but I wish you a Happy New Year too!");
+            }
+        }catch (Exception e){
+            return INCORRECT_MESSAGE;
+        }
+        return String.join(SPACE, firstName, WISH);
+    }
+
+}
---
+++ src/happy_new_year/Main.java
@@ -0,0 +1,25 @@
+package happy_new_year;
+import java.util.Scanner;
+
+public class Main {
+    private static final String BLUE_PRINT = "\u001B[34m";
+
+    public static void main(String[] args) {
+        boolean exit = false;
+
+        while (!exit){
+            System.out.println(BLUE_PRINT + "If you Valentin enter 1, if you Elijah enter 2. \n" +
+                                            "If you want to go out enter 0.");
+            Scanner scan = new Scanner(System.in);
+            int input = scan.nextInt();
+            if (input != 0){
+                String output = new Friend().greeting(input);
+                System.out.println(output);
+            }else{
+                System.out.println("Bye - bye");
+                exit = true;
+            }
+        }
+    }
+
+}
